1696fddf8878f0680d5b9a2e485fc34d3eedf4dd
Dwesh163 Desktop <ehouarn.duriaux@protonmail.com>
2024-01-05 23:41:12 +0100

    [add] react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { Header } from './components/HeaderComponents.jsx';
+
+function App() {
+	return (
+		<>
+			<Header />
+		</>
+	);
+}
+
+export default App;
