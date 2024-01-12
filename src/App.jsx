179b1b340440c6a17253806d9fe3ee953c2a5dbc
Dwesh163 Desktop <ehouarn.duriaux@protonmail.com>
2024-01-12 22:38:05 +0100

    [add] recent

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import { Menu } from './components/menuComponents.jsx';
+import { Content } from './components/contentComponents.jsx';
 
 function App() {
 	return (
 		<>
 			<Menu />
+			<Content />
 		</>
 	);
 }
---
+++ src/components/content/Recent.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+
+export function Recent() {
+	return <h3>your recent movie</h3>;
+}
